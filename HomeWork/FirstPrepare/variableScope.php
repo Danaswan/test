0e0c6eb8a1d988ea1461ba2735f9796432d472d6
vagrant <vagrant@homestead>
2019-11-30 09:45:39 +0000

    firstPrepare

---
+++ HomeWork/FirstPrepare/variableScope.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZhouFurong
+ * Date: 2019/11/30
+ * Time: 9:47
+ */
+
+// 局部变量
+
+$a = 3;
+function num(){
+    echo $a;
+}
+
+num();
+
+?>
+
+<script type="application/javascript">
+    var a = 123;
+    function myFunction(){
+        console.log(a);
+    }
+    myFunction();
+</script>
+
